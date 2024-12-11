fabc0dc50a7374cd7765b88293120b1f880f51b6
abdelhamiddsalah <abdelhamidsalah222@gmail.com>
2024-12-11 13:55:07 +0200

    bottomnav

--- lib/core/widgets/bottom_nav.dart
+++ lib/core/widgets/bottom_nav.dart
@@ -13,7 +13,7 @@ class BottomNav extends StatefulWidget {
 class _BottomNavState extends State<BottomNav> {
   int _currentIndex = 0;
 
-  // الصفحات التي سيتم عرضها
+
   final List<Widget> _pages = [
     const HomeView(),
     Center(child: Text('Search Page')),
@@ -24,7 +24,7 @@ class _BottomNavState extends State<BottomNav> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _pages[_currentIndex], // الصفحة النشطة
+      body: _pages[_currentIndex],  
       bottomNavigationBar: ClipRRect(
         borderRadius: const BorderRadius.only(
           topLeft: Radius.circular(20),
@@ -35,28 +35,28 @@ class _BottomNavState extends State<BottomNav> {
           type: BottomNavigationBarType.fixed,
           selectedItemColor: Colors.black,
           unselectedItemColor: Colors.grey,
-          currentIndex: _currentIndex, // العنصر النشط
+          currentIndex: _currentIndex, 
           onTap: (index) {
             setState(() {
-              _currentIndex = index; // تغيير الصفحة عند النقر
+              _currentIndex = index; 
             });
           },
           items: const [
             BottomNavigationBarItem(
               icon: Icon(Icons.home),
-              label: '', // تعيين قيمة فارغة للـ label
+              label: '', 
             ),
             BottomNavigationBarItem(
               icon: Icon(Icons.shopping_bag_outlined),
-              label: '', // تعيين قيمة فارغة للـ label
+              label: '', 
             ),
             BottomNavigationBarItem(
               icon: Icon(Icons.payment),
-              label: '', // تعيين قيمة فارغة للـ label
+              label: '',    
             ),
             BottomNavigationBarItem(
               icon: Icon(Icons.perm_contact_cal_outlined),
-              label: '', // تعيين قيمة فارغة للـ label
+              label: '', 
             ),
           ],
         ),
