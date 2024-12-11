f0e7fcaf0c8d89ffb60ff00fead8ac69ae13956e
abdelhamiddsalah <abdelhamidsalah222@gmail.com>
2024-12-11 12:55:45 +0200

    home

--- lib/Feature/home/views/widgets/home_view_body.dart
+++ lib/Feature/home/views/widgets/home_view_body.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shoes/Feature/home/views/widgets/appbar_in_home.dart';
+import 'package:shoes/Feature/home/views/widgets/textfield_in_home.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-     
+    return  Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding:  EdgeInsets.symmetric(vertical: 43.h,horizontal: 20.w),
+        child: Column(
+          children: [
+            const AppbarInHome(),
+            SizedBox(height: 30.h,),
+            const TextfieldInHome(),
+          ],
+        ),
+      ),
     );
   }
 }
