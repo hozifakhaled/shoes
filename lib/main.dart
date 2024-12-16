e3061a9a02ae658fc298363f36a3a270972dd68e
abdelhamiddsalah <abdelhamidsalah222@gmail.com>
2024-12-16 08:44:41 +0200

    ch

--- lib/main.dart
+++ lib/main.dart
@@ -3,36 +3,38 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:shoes/Feature/onboarding/views/onboardingPage.dart';
-import 'package:shoes/Feature/stripe_payment/views/widgets/payment_keys.dart';
 import 'package:shoes/core/routing/app_routing.dart';
 
-
-void main() async{
-  Stripe.publishableKey=PaymentKeys.publisherKey;
-  await  dotenv.load(); 
-  runApp(ShosesApp(appRouting: AppRouting(),));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(); // تحميل البيئة من ملف .env
+  if (dotenv.env.containsKey('PUBLISHER_KEY')) {
+    debugPrint("Environment loaded successfully!");
+  } else {
+    debugPrint("Failed to load environment.");
+  }
+  Stripe.publishableKey = dotenv.env['PUBLISHER_KEY'] ?? 'default_key'; // تعيين مفتاح النشر من البيئة
+  runApp(ShosesApp(appRouting: AppRouting()));
 }
 
 class ShosesApp extends StatelessWidget {
   const ShosesApp({super.key, required this.appRouting});
   final AppRouting appRouting;
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-        designSize: const Size(360, 690),
-        minTextAdapt: true,
-        splitScreenMode: true,
-        builder: (_, child) {
-          return  MaterialApp(
-            debugShowCheckedModeBanner: false,
-            title: 'Flutter Demo',
-            home:Onboardingpage(),
-
-            onGenerateRoute: appRouting.generateRoute,
-
-          );
-        });
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Demo',
+          home: Onboardingpage(),
+          onGenerateRoute: appRouting.generateRoute,
+        );
+      },
+    );
   }
 }
-
